ad21a5058a9844eb38950db5a598f60159bef1d8
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-12 17:57:35 +1000

    Create project file structure

---
+++ src/Bitcoin.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import data from "./data";
+
+const Bitcoin = () => {
+  return (
+    <div className="bitcoin">
+      <p className="desc">
+        Find out how much more money you would have if you bought $10,000 USD
+        bitcoin, rather than any of the tech IPOs from the last decade.{" "}
+      </p>
+      <button className="btn">I'm an idiot</button>
+    </div>
+  );
+};
+
+export default Bitcoin;
