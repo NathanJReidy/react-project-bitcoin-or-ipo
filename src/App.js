8d9fe62f211e5d69b621c32a7a8b6061a8d52b6c
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-12 19:18:31 +1000

    added css changes and layout changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
     <main>
       <section className="container">
         <h2 className="title">Bitcoin or IPO</h2>
+        <p className="desc">
+          Calculate how much more money you would have if you bought bitcoin
+          instead of another dumb tech IPO.
+        </p>
+        <div className="investmentContainer">
+          <h4 className="initialInvestment">Initial Investment: </h4>$
+          <input
+            className="investmentAmount"
+            placeholder="Enter USD amount"
+          ></input>
+        </div>
+
         <Bitcoin />
       </section>
     </main>
--- src/Bitcoin.js
+++ src/Bitcoin.js
@@ -4,10 +4,25 @@ import data from "./data";
 const Bitcoin = () => {
   return (
     <div className="bitcoin">
-      <p className="desc">
-        Find out how much more money you would have if you bought $10,000 USD
-        bitcoin, rather than any of the tech IPOs from the last decade.{" "}
-      </p>
+      <div className="imgContainer">
+        <img
+          className="img"
+          src="https://icons.iconarchive.com/icons/papirus-team/papirus-apps/256/bitcoin-icon.png"
+        ></img>
+        <h4 className="vs">VS</h4>
+        <img
+          className="img"
+          src="https://www.flaticon.com/svg/vstatic/svg/2111/2111742.svg?token=exp=1618219927~hmac=14663d3afc7de46a4822c6c1e32f129c"
+        ></img>
+      </div>
+
+      <h3 className="calcContainer">
+        If you bought <h3 className="btc"> &ensp;$10,000 of bitcoin &ensp; </h3>{" "}
+        instead of
+        <h3 className="company"> &ensp; Uber's IPO &ensp; </h3>You would have an
+        extra
+        <h3 className="oppCost"> &ensp; $100,000 &ensp; </h3>
+      </h3>
       <button className="btn">I'm an idiot</button>
     </div>
   );
